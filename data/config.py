36f5bf0b7c2e86002755ac61ee0ac8afc9a23f95
FedosOri <94121264+FedosOri@users.noreply.github.com>
2022-05-04 12:40:18 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,8 +1,7 @@
-token = "5330820554:AAEcUbb6DUA181uqyQ1iPPh0cDP4N8NZnow"
-channel = "@testorikkk"
-super_admins = ["morozori", "Bolshoy_molodec"]
-host = "rc1c-zoljztvznd89cadv.mdb.yandexcloud.net"
-port = "6432"
-user = "MorozOri"
-password = "az09az09"
-# database = "ColdBase"
+token = ""
+channel = ""
+super_admins = []
+host = ""
+port = ""
+user = ""
+password = ""
